17946f0e6a75af091f2d47cf91274e1dcea3d2fa
juanb <juanbucio303@outlook.com>
2021-02-01 16:12:56 -0600

    first commit twfsdaws

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,35 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
+
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/5.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.0/init.js');
+ 
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
 firebase.initializeApp({
-   apiKey: "AIzaSyA5VKKM9a6EBaXLGvLRyjCLS5-KA3c4r4w",
-   authDomain: "notificationtest-6d60f.firebaseapp.com",
-   databaseURL: "https://notificationtest-6d60f.firebaseio.com",
-   projectId: "notificationtest-6d60f",
-   storageBucket: "notificationtest-6d60f.appspot.com",
-   messagingSenderId: "577427666180",
-   appId: "1:577427666180:web:048e25d46ba14509eccc51",
-   measurementId: "G-J3BZG6VZLM"
+  'messagingSenderId': '*****'
 });
-
+ 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
+// [END initialize_firebase_in_sw]
+ 
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.'
+  };
+ 
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
