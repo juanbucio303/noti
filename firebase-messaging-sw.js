55418d31862c54f6d7f4754ea2607c9c804a2719
juanb <juanbucio303@outlook.com>
2021-02-01 15:40:53 -0600

    first commit 4

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,11 +3,14 @@ importScripts('https://www.gstatic.com/firebasejs/3.7.1/firebase-messaging.js');
 
 // Initialize Firebase
   var config = {
-     apiKey: "AIzaSyA5VKKM9a6EBaXLGvLRyjCLS5-KA3c4r4w",
-     authDomain: "notificationtest-6d60f.firebaseapp.com",
-     databaseURL: "https://notificationtest-6d60f.firebaseio.com",
-     storageBucket: "notificationtest-6d60f.appspot.com",
-     messagingSenderId: "577427666180",
+   apiKey: "AIzaSyA5VKKM9a6EBaXLGvLRyjCLS5-KA3c4r4w",
+   authDomain: "notificationtest-6d60f.firebaseapp.com",
+   databaseURL: "https://notificationtest-6d60f.firebaseio.com",
+   projectId: "notificationtest-6d60f",
+   storageBucket: "notificationtest-6d60f.appspot.com",
+   messagingSenderId: "577427666180",
+   appId: "1:577427666180:web:e0a7a52517085658eccc51",
+   measurementId: "G-8E25M7K4V5"
   };
   firebase.initializeApp(config);
   
