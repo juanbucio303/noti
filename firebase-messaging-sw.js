156e361099a160e485b892cd5141e40ca30d8541
juanb <juanbucio303@outlook.com>
2021-02-01 15:42:20 -0600

    first commit 3

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.0/init.js');
 
 // Initialize Firebase
   var config = {
